9931dd578a1deab9d80f45995d5fe04ecd7e9264
Sl0wYx <lol-waring14@mail.ru>
2026-03-26 01:13:13 +0000

    Telegram message parsing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, Depends
+
 from app.routers import accounts
 from app.routers import stats
 from app.routers import online
+from app.routers import recieve_message
 app = FastAPI()
 
 app.include_router(accounts.router)
 app.include_router(stats.router)
 app.include_router(online.router)
+app.include_router(recieve_message.router)
+
+
+
---
+++ app/routers/recieve_message.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request, HTTPException
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv("app/data/private")
+
+API_TOKEN = os.getenv("API_TOKEN")
+router = APIRouter()
+
+@router.get("/get_message", tags=["telegram"])
+async def get_message():
+    with open("app/data/message.json", mode='r', encoding='utf-8-sig') as message:
+        return json.load(message)
+
+@router.post('/receive_message', tags=["telegram"])
+async def receive_message(data: dict, request: Request):
+    if request.headers.get("Authorization") != API_TOKEN:
+        raise HTTPException(status_code=401, detail="Invalid API Token")
+    else:
+        base = Path("app/data")
+        path = base / "message.json"
+        path.write_text(json.dumps(data))
